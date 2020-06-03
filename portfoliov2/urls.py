aa2976dd4a2938985640cf003bc373a6c82a8f81
Robert Villazana <r.villazana@yahoo.com>
2020-06-03 14:45:30 -0500

    added about me page

--- portfoliov2/urls.py
+++ portfoliov2/urls.py
@@ -8,7 +8,8 @@ from portfolio import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+    path('about/', views.about, name='about')
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
